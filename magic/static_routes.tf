09e60745697fcc0aed634f6ec57b87cf0b5392d4
Erfi Anugrah <erfi.anugrah@gmail.com>
2024-05-08 11:13:11 +0000

    add eth2 to static routes

--- magic/static_routes.tf
+++ magic/static_routes.tf
@@ -14,10 +14,19 @@ resource "cloudflare_static_route" "eth1_100_vyos_nl_ipsec" {
   priority    = 100
 }
 
+
 resource "cloudflare_static_route" "podman_vyos_nl_ipsec" {
   account_id  = var.cloudflare_account_id
   description = "Podman"
   prefix      = "172.18.0.0/16"
+  nexthop     = "10.68.100.20"
+  priority    = 100
+}
+
+resource "cloudflare_static_route" "eth2_vyos_nl_ipsec" {
+  account_id  = var.cloudflare_account_id
+  description = "ETH2"
+  prefix      = "10.72.0.0/16"
   nexthop     = "10.68.100.20"
   priority    = 100
 }
