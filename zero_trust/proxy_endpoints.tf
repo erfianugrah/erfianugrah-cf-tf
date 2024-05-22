e2553733e64d2e6150ae398e00cb09fbd8afbb62
Erfi Anugrah <erfi.anugrah@gmail.com>
2024-05-22 05:20:28 +0000

    pac file IPs

--- zero_trust/proxy_endpoints.tf
+++ zero_trust/proxy_endpoints.tf
@@ -1,11 +1,12 @@
 resource "cloudflare_teams_proxy_endpoint" "nl_proxy_endpoint" {
   account_id = var.cloudflare_account_id
   name       = "nl"
-  ips        = ["10.68.69.1/32"]
+  ips        = ["${var.nl_ip}/32"]
 }
 
 resource "cloudflare_teams_proxy_endpoint" "sg_proxy_endpoint" {
   account_id = var.cloudflare_account_id
   name       = "sg"
-  ips        = ["10.68.69.1/32"]
+  ips        = ["${var.sg_ip}/32"]
 }
+
