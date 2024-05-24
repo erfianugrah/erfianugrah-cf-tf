2dbb42d3019997d9abf260df21a6a900008496ca
Erfi Anugrah <erfi.anugrah@gmail.com>
2024-05-24 12:31:19 +0000

    added vyos-ssh

--- main_zone/dns.tf
+++ main_zone/dns.tf
@@ -924,3 +924,12 @@ resource "cloudflare_record" "ollama-ui" {
   value   = var.sg_ip
   zone_id = var.cloudflare_zone_id
 }
+
+resource "cloudflare_record" "vyos-ssh-nl" {
+  name    = "vyos-ssh"
+  proxied = true
+  ttl     = 1
+  type    = "CNAME"
+  value   = cloudflare_tunnel.vyos_nl.cname
+  zone_id = var.cloudflare_zone_id
+}
