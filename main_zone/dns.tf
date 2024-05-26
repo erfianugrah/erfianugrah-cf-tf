23f50dabd3c3b735439a83736dd1dd06030aa6d3
Erfi Anugrah <erfi.anugrah@gmail.com>
2024-05-26 17:34:18 +0000

    test jellyfin on ip

--- main_zone/dns.tf
+++ main_zone/dns.tf
@@ -246,8 +246,8 @@ resource "cloudflare_record" "jellyfin" {
   proxied = true
   tags    = ["servarr"]
   ttl     = 1
-  type    = "CNAME"
-  value   = cloudflare_tunnel.servarr.cname
+  type    = "A"
+  value   = var.sg_ip 
   zone_id = var.cloudflare_zone_id
 }
 
