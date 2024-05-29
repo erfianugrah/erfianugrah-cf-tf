4ef0a58941031bb62b3009c8e8d6866bc863fc82
Erfi Anugrah <erfi.anugrah@gmail.com>
2024-05-29 13:58:28 +0000

    split tunnel

--- zero_trust/split_tunnel.tf
+++ zero_trust/split_tunnel.tf
@@ -1,35 +1,27 @@
 resource "cloudflare_split_tunnel" "default_include" {
   account_id = var.cloudflare_account_id
   policy_id  = cloudflare_device_settings_policy.default.id
-  mode       = "include"
+  mode       = "exclude"
   tunnels {
-    address     = "10.68.0.0/16"
+    address     = "10.68.69.3/32"
     description = "Home"
   }
   tunnels {
-    address     = "10.0.0.0/8"
-    description = "Local"
-  }
-  tunnels {
-    address     = "172.18.0.8/32"
-    description = "docker-podman"
+    address     = "172.18.0.2/32"
+    description = "pihole"
   }
 }
 
 resource "cloudflare_split_tunnel" "google_include" {
   account_id = var.cloudflare_account_id
   policy_id  = cloudflare_device_settings_policy.google.id
-  mode       = "include"
+  mode       = "exclude"
   tunnels {
-    address     = "10.68.0.0/16"
+    address     = "10.68.69.3/32"
     description = "Home"
   }
   tunnels {
-    address     = "10.0.0.0/8"
-    description = "Local"
-  }
-  tunnels {
-    address     = "172.18.0.8/32"
-    description = "docker-podman"
+    address     = "172.18.0.2/32"
+    description = "pihole"
   }
 }
