94b9d45bbfe27df9e6e3306ef231959e670c60ad
Erfi Anugrah <erfi.anugrah@gmail.com>
2024-06-12 15:19:35 +0000

    add new fields to logpush

--- main_zone/zone_logpush_ownership.tf
+++ main_zone/zone_logpush_ownership.tf
@@ -1,4 +1,9 @@
 resource "cloudflare_logpush_ownership_challenge" "spectrum_gcs" {
   zone_id          = var.cloudflare_zone_id
+  destination_conf = "gs://erfi-logs/spectrum"
+}
+
+resource "cloudflare_logpush_ownership_challenge" "http_gcs" {
+  zone_id          = var.cloudflare_zone_id
   destination_conf = "gs://erfi-logs/http"
 }
