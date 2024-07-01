f420c5572bf71e8362d4d6116595f1f0160077e5
Erfi Anugrah <erfi.anugrah@gmail.com>
2024-07-01 07:29:58 +0000

    exclude windows PC only

--- zero_trust/split_tunnel.tf
+++ zero_trust/split_tunnel.tf
@@ -1,27 +1,27 @@
-resource "cloudflare_split_tunnel" "default_include" {
+resource "cloudflare_split_tunnel" "default_exclude" {
   account_id = var.cloudflare_account_id
   policy_id  = cloudflare_device_settings_policy.default.id
   mode       = "exclude"
   tunnels {
-    address     = "10.68.69.3/32"
+    address     = "10.0.69.3/32"
     description = "Home"
   }
-  tunnels {
-    address     = "172.18.0.2/32"
-    description = "pihole"
-  }
+  # tunnels {
+  #   address     = "172.18.0.2/32"
+  #   description = "pihole"
+  # }
 }
 
-resource "cloudflare_split_tunnel" "google_include" {
+resource "cloudflare_split_tunnel" "google_exclude" {
   account_id = var.cloudflare_account_id
   policy_id  = cloudflare_device_settings_policy.google.id
   mode       = "exclude"
   tunnels {
-    address     = "10.68.69.3/32"
+    address     = "10.0.69.3/32"
     description = "Home"
   }
-  tunnels {
-    address     = "172.18.0.2/32"
-    description = "pihole"
-  }
+  # tunnels {
+  #   address     = "172.18.0.2/32"
+  #   description = "pihole"
+  # }
 }
