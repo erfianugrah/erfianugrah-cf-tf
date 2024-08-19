6eda84d7c6fdd06b806d3a20275f86717c4b58ce
Erfi Anugrah <erfi.anugrah@gmail.com>
2024-08-19 08:56:33 +0000

    update web analytics

--- main_zone/web_analytics_rule.tf
+++ main_zone/web_analytics_rule.tf
@@ -1,9 +1,10 @@
-resource "cloudflare_web_analytics_rule" "root" {
-  depends_on = [cloudflare_web_analytics_site.root]
-  account_id = var.cloudflare_account_id
-  ruleset_id = cloudflare_web_analytics_site.root.ruleset_id
-  host       = "*"
-  paths      = ["*"]
-  inclusive  = false
-  is_paused  = false
-}
+# resource "cloudflare_web_analytics_rule" "root" {
+#   depends_on = [cloudflare_web_analytics_site.root]
+#   account_id = var.cloudflare_account_id
+#   ruleset_id = cloudflare_web_analytics_site.root.ruleset_id
+#   host       = "*"
+#   paths      = ["*"]
+#   inclusive  = true
+#   is_paused  = false
+# }
+#
