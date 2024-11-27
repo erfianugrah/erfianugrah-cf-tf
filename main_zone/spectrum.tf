32992b7737c4e0d8f496de53c120c7ced7d886a6
Erfi Anugrah <erfi.anugrah@gmail.com>
2024-11-27 10:26:56 +0000

    tested spectrum leased IPs

--- main_zone/spectrum.tf
+++ main_zone/spectrum.tf
@@ -1,5 +1,5 @@
 resource "cloudflare_spectrum_application" "coturn_tcp" {
-  origin_direct  = ["tcp://118.189.189.102:3478"]
+  origin_direct  = ["tcp://${var.sg_ip}:3478"]
   protocol       = "tcp/3478"
   proxy_protocol = "v2"
   tls            = "off"
@@ -16,7 +16,7 @@ resource "cloudflare_spectrum_application" "coturn_tcp" {
 }
 
 resource "cloudflare_spectrum_application" "coturn_udp" {
-  origin_direct  = ["udp://118.189.189.102:3478"]
+  origin_direct  = ["udp://${var.sg_ip}:3478"]
   protocol       = "udp/3478"
   proxy_protocol = "v2"
   tls            = "off"
@@ -33,7 +33,7 @@ resource "cloudflare_spectrum_application" "coturn_udp" {
 }
 
 resource "cloudflare_spectrum_application" "coturn_tcp_tls" {
-  origin_direct  = ["tcp://118.189.189.102:5349"]
+  origin_direct  = ["tcp://${var.sg_ip}:5349"]
   protocol       = "tcp/5349"
   proxy_protocol = "v2"
   tls            = "off"
@@ -50,7 +50,7 @@ resource "cloudflare_spectrum_application" "coturn_tcp_tls" {
 }
 
 resource "cloudflare_spectrum_application" "coturn_udp_tls" {
-  origin_direct  = ["udp://118.189.189.102:5349"]
+  origin_direct  = ["udp://${var.sg_ip}:5349"]
   protocol       = "udp/5349"
   proxy_protocol = "v2"
   tls            = "off"
@@ -67,7 +67,7 @@ resource "cloudflare_spectrum_application" "coturn_udp_tls" {
 }
 
 resource "cloudflare_spectrum_application" "coturn_ephemeral" {
-  origin_direct  = ["udp://118.189.189.102:49152-65535"]
+  origin_direct  = ["udp://${var.sg_ip}:49152-65535"]
   protocol       = "udp/49152-65535"
   proxy_protocol = "v2"
   tls            = "off"
@@ -82,6 +82,49 @@ resource "cloudflare_spectrum_application" "coturn_ephemeral" {
     type         = "dynamic"
   }
 }
+
+resource "cloudflare_spectrum_application" "static_ip_test_1" {
+  origin_direct  = ["tcp://${var.sg_ip}:7478"]
+  protocol       = "tcp/7478"
+  proxy_protocol = "v2"
+  tls            = "off"
+  traffic_type   = "direct"
+  zone_id        = var.cloudflare_zone_id
+  dns {
+    name = "static1.${var.domain_name}"
+    type = "ADDRESS"
+  }
+  edge_ips {
+    # connectivity = "ipv4"
+    type = "static"
+    ips = [
+      var.spectrum_static_v6_1,
+      var.spectrum_static_v6_2
+    ]
+  }
+}
+
+resource "cloudflare_spectrum_application" "static_ip_test_2" {
+  origin_direct  = ["udp://${var.sg_ip}:6478"]
+  protocol       = "udp/6478"
+  proxy_protocol = "v2"
+  tls            = "off"
+  traffic_type   = "direct"
+  zone_id        = var.cloudflare_zone_id
+  dns {
+    name = "static1.${var.domain_name}"
+    type = "ADDRESS"
+  }
+  edge_ips {
+    # connectivity = "ipv4"
+    type = "static"
+    ips = [
+      var.spectrum_static_v6_1,
+      var.spectrum_static_v6_2
+    ]
+  }
+}
+
 
 # resource "cloudflare_spectrum_application" "same_hostname_test_1" {
 #   origin_direct = ["tcp://118.189.189.102:443"]
--- main_zone/variables.tf
+++ main_zone/variables.tf
@@ -46,6 +46,18 @@ variable "nl_ip" {
   sensitive   = true
 }
 
+variable "spectrum_static_v6_1" {
+  description = "spectrum static IP lease"
+  type        = string
+  sensitive   = true
+}
+
+variable "spectrum_static_v6_2" {
+  description = "spectrum static IP lease"
+  type        = string
+  sensitive   = true
+}
+
 variable "domain_name" {
   description = "The domain name to be used"
   type        = string
@@ -60,95 +72,95 @@ variable "secondary_domain_name" {
 }
 variable "joplin_pem_path" {
   description = "joplin_pem_path"
-  type = string
+  type        = string
 }
 
 variable "joplin_key_path" {
   description = "joplin_key_path"
-  type = string
+  type        = string
 }
 
 variable "privatebin_pem_path" {
   description = "privatebin_pem_path"
-  type = string
+  type        = string
 }
 
 variable "privatebin_key_path" {
   description = "privatebin_key_path"
-  type = string
+  type        = string
 }
 
 variable "vaultwarden_pem_path" {
   description = "vaultwarden_pem_path"
-  type = string
+  type        = string
 }
 
 variable "vaultwarden_key_path" {
   description = "vaultwarden_key_path"
-  type = string
+  type        = string
 }
 
 variable "httpbun_ca_pem_path" {
   description = "httpbun_ca_pem_path"
-  type = string
+  type        = string
 }
 
 variable "httpbun_ca_key_path" {
   description = "httpbun_ca_key_path"
-  type = string
+  type        = string
 }
 
 variable "immich_pem_path" {
   description = "immich_pem_path"
-  type = string
+  type        = string
 }
 
 variable "immich_key_path" {
   description = "immich_key_path"
-  type = string
+  type        = string
 }
 
 variable "file_pem_path" {
   description = "file_pem_path"
-  type = string
+  type        = string
 }
 
 variable "file_key_path" {
   description = "file_key_path"
-  type = string
+  type        = string
 }
 
 variable "caddy_pem_path" {
   description = "caddy_pem_path"
-  type = string
+  type        = string
 }
 
 variable "caddy_key_path" {
   description = "caddy_key_path"
-  type = string
+  type        = string
 }
 
 variable "pages_domain" {
   description = "pages domain"
-  type = string
+  type        = string
 }
 
 variable "deno_domain" {
   description = "deno domain"
-  type = string
+  type        = string
 }
 
 variable "k3s_tunnel" {
   description = "k3s tunnel cname"
-  type = string
+  type        = string
 }
 
 variable "r2_access_key_id" {
   description = "r2 access key id"
-  type = string
+  type        = string
 }
 
 variable "r2_access_key" {
   description = "r2 access key"
-  type = string
+  type        = string
 }
