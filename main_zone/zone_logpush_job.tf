52c1fe4baeacb8ba8fdc721777f39c83b688237c
Erfi Anugrah <erfi.anugrah@gmail.com>
2025-03-05 08:41:07 +0000

    added more fields

--- main_zone/zone_logpush_job.tf
+++ main_zone/zone_logpush_job.tf
@@ -3,8 +3,49 @@ resource "cloudflare_logpush_job" "http_r2" {
   destination_conf            = "r2://http-logs/{DATE}?access-key-id=${var.r2_access_key_id}&secret-access-key=${var.r2_access_key}&account-id=${var.cloudflare_account_id}"
   enabled                     = true
   max_upload_interval_seconds = "30"
-  logpull_options             = "fields=ClientIP,ClientRequestHost,ClientRequestMethod,ClientRequestURI,EdgeEndTimestamp,EdgeResponseBytes,EdgeResponseStatus,EdgeStartTimestamp,RayID,BotScore,BotScoreSrc,BotTags,CacheCacheStatus,CacheResponseBytes,CacheResponseStatus,CacheTieredFill,ClientASN,ClientCountry,ClientDeviceType,ClientIPClass,ClientMTLSAuthCertFingerprint,ClientMTLSAuthStatus,ClientRequestBytes,ClientRequestPath,ClientRequestProtocol,ClientRequestReferer,ClientRequestScheme,ClientRequestSource,ClientRequestUserAgent,ClientSSLCipher,ClientSSLProtocol,ClientSrcPort,ClientTCPRTTMs,ClientXRequestedWith,Cookies,EdgeCFConnectingO2O,EdgeColoCode,EdgeColoID,EdgePathingOp,EdgePathingSrc,EdgePathingStatus,EdgeRateLimitAction,EdgeRateLimitID,EdgeRequestHost,EdgeResponseBodyBytes,EdgeResponseCompressionRatio,EdgeResponseContentType,EdgeServerIP,EdgeTimeToFirstByteMs,FirewallMatchesActions,FirewallMatchesRuleIDs,FirewallMatchesSources,JA3Hash,OriginDNSResponseTimeMs,OriginIP,OriginRequestHeaderSendDurationMs,OriginResponseBytes,OriginResponseDurationMs,OriginResponseHTTPExpires,OriginResponseHTTPLastModified,OriginResponseHeaderReceiveDurationMs,OriginResponseStatus,OriginResponseTime,OriginSSLProtocol,OriginTCPHandshakeDurationMs,OriginTLSHandshakeDurationMs,ParentRayID,RequestHeaders,ResponseHeaders,SecurityLevel,SmartRouteColoID,UpperTierColoID,WAFAction,WAFFlags,WAFMatchedVar,WAFProfile,WAFRuleID,WAFRuleMessage,WorkerCPUTime,WorkerStatus,WorkerSubrequest,WorkerSubrequestCount,ZoneID,ZoneName,CacheReserveUsed,WorkerWallTimeUs,BotDetectionIDs,ClientRegionCode,ContentScanObjResults,ContentScanObjTypes,WAFAttackScore,WAFRCEAttackScore,WAFSQLiAttackScore,WAFXSSAttackScore,SecurityAction,SecurityActions,SecurityRuleDescription,SecurityRuleID,SecurityRuleIDs,SecuritySources,Action,ClientASNDescription,ClientRefererHost,ClientRefererPath,ClientRefererQuery,ClientRefererScheme,ClientRequestQuery,Datetime,Description,Kind,MatchIndex,Metadata,OriginatorRayID,Ref,RuleID,Source,LeakedCredentialCheckResult&timestamps=rfc3339&CVE-2021-44228=false"
-  zone_id                     = var.cloudflare_zone_id
+
+  output_options {
+    output_type = "ndjson"
+    field_names = [
+      "ClientIP", "ClientRequestHost", "ClientRequestMethod", "ClientRequestURI",
+      "EdgeEndTimestamp", "EdgeResponseBytes", "EdgeResponseStatus", "EdgeStartTimestamp",
+      "RayID", "BotScore", "BotScoreSrc", "BotTags", "CacheCacheStatus",
+      "CacheResponseBytes", "CacheResponseStatus", "CacheTieredFill", "ClientASN",
+      "ClientCountry", "ClientDeviceType", "ClientIPClass", "ClientMTLSAuthCertFingerprint",
+      "ClientMTLSAuthStatus", "ClientRequestBytes", "ClientRequestPath",
+      "ClientRequestProtocol", "ClientRequestReferer", "ClientRequestScheme",
+      "ClientRequestSource", "ClientRequestUserAgent", "ClientSSLCipher",
+      "ClientSSLProtocol", "ClientSrcPort", "ClientTCPRTTMs", "ClientXRequestedWith",
+      "Cookies", "EdgeCFConnectingO2O", "EdgeColoCode", "EdgeColoID", "EdgePathingOp",
+      "EdgePathingSrc", "EdgePathingStatus", "EdgeRateLimitAction", "EdgeRateLimitID",
+      "EdgeRequestHost", "EdgeResponseBodyBytes", "EdgeResponseCompressionRatio",
+      "EdgeResponseContentType", "EdgeServerIP", "EdgeTimeToFirstByteMs",
+      "FirewallMatchesActions", "FirewallMatchesRuleIDs", "FirewallMatchesSources",
+      "JA3Hash", "OriginDNSResponseTimeMs", "OriginIP", "OriginRequestHeaderSendDurationMs",
+      "OriginResponseBytes", "OriginResponseDurationMs", "OriginResponseHTTPExpires",
+      "OriginResponseHTTPLastModified", "OriginResponseHeaderReceiveDurationMs",
+      "OriginResponseStatus", "OriginResponseTime", "OriginSSLProtocol",
+      "OriginTCPHandshakeDurationMs", "OriginTLSHandshakeDurationMs", "ParentRayID",
+      "RequestHeaders", "ResponseHeaders", "SecurityLevel", "SmartRouteColoID",
+      "UpperTierColoID", "WAFAction", "WAFFlags", "WAFMatchedVar", "WAFProfile",
+      "WAFRuleID", "WAFRuleMessage", "WorkerCPUTime", "WorkerStatus", "WorkerSubrequest",
+      "WorkerSubrequestCount", "ZoneID", "ZoneName", "CacheReserveUsed", "WorkerWallTimeUs",
+      "BotDetectionIDs", "ClientRegionCode", "ContentScanObjResults", "ContentScanObjTypes",
+      "WAFAttackScore", "WAFRCEAttackScore", "WAFSQLiAttackScore", "WAFXSSAttackScore",
+      "SecurityAction", "SecurityActions", "SecurityRuleDescription", "SecurityRuleID",
+      "SecurityRuleIDs", "SecuritySources", "Action", "ClientASNDescription",
+      "ClientRefererHost", "ClientRefererPath", "ClientRefererQuery", "ClientRefererScheme",
+      "ClientRequestQuery", "Datetime", "Description", "Kind", "MatchIndex", "Metadata",
+      "OriginatorRayID", "Ref", "RuleID", "Source", "LeakedCredentialCheckResult",
+      "BotDetectionTags", "ClientCity", "ClientLatitude", "ClientLongitude",
+      "ContentScanObjSizes", "JA4", "JA4Signals", "ParentRayID", "RayID",
+      "WorkerScriptName"
+    ]
+    timestamp_format = "rfc3339"
+    cve20214428      = false
+  }
+
+  zone_id = var.cloudflare_zone_id
 }
 
 # resource "cloudflare_logpush_job" "firewall_gcs" {
