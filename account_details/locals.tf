1de9c1774f6e714be1f99707b630c94e2d931c9e
Erfi Anugrah <erfi.anugrah@gmail.com>
2025-11-26 20:25:49 +0100

    added error alert

--- account_details/locals.tf
+++ account_details/locals.tf
@@ -6,14 +6,10 @@ locals {
 }
 
 locals {
-  first_response = jsondecode(data.http.first_page.response_body)
-
-  # Check if request was successful
-  success       = try(local.first_response.success, false)
-  first_results = local.success ? try(local.first_response.result, []) : []
-  total_pages   = local.success ? try(local.first_response.result_info.total_pages, 1) : 0
-
-  # Create list of remaining pages (2, 3, 4, etc.)
+  first_response  = jsondecode(data.http.first_page.response_body)
+  success         = try(local.first_response.success, false)
+  first_results   = try([for r in local.first_response.result : r], [])
+  total_pages     = try(local.first_response.result_info.total_pages, 0)
   remaining_pages = local.total_pages > 1 ? range(2, local.total_pages + 1) : []
 }
 
@@ -22,7 +18,5 @@ locals {
     for page in data.http.remaining_pages :
     try(jsondecode(page.response_body).result, [])
   ])
-
   accounts = concat(local.first_results, local.remaining_results)
 }
-
