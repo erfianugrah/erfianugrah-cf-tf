f58539c10389af6c6e969bafd1a8a9a3d3603869
Erfi Anugrah <erfi.anugrah@gmail.com>
2026-06-22 10:20:32 +0800

    manage all zones and set erfi.io to partial

--- main_zone/zone.tf
+++ main_zone/zone.tf
@@ -5,3 +5,19 @@ resource "cloudflare_zone" "erfianugrah" {
   type       = "full"
   zone       = var.domain_name
 }
+
+resource "cloudflare_zone" "erfi_dev" {
+  account_id = var.cloudflare_account_id
+  paused     = false
+  plan       = "enterprise"
+  type       = "full"
+  zone       = var.secondary_domain_name
+}
+
+resource "cloudflare_zone" "erfi_io" {
+  account_id = var.cloudflare_account_id
+  paused     = false
+  plan       = "enterprise"
+  type       = "partial"
+  zone       = var.tertiary_domain_name
+}
